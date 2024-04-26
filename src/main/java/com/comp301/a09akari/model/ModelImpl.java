029588708866667030c813487a8b2b7e995646e4
KushG04 <kushg@unc.edu>
2024-04-25 22:14:31 -0400

    fourteenth submission

--- src/main/java/com/comp301/a09akari/model/ModelImpl.java
+++ src/main/java/com/comp301/a09akari/model/ModelImpl.java
@@ -74,23 +74,27 @@ public class ModelImpl implements Model {
       return true;
     }
 
-    for (int i = r; i >= 0; i--) {
-      if (getActivePuzzle().getCellType(i, c) != CellType.WALL && lamps[i][c]) {
+    for (int i = r; i >= 0 && getActivePuzzle().getCellType(i, c) != CellType.WALL; i--) {
+      if (lamps[i][c]) {
         return true;
       }
     }
-    for (int i = r; i < getActivePuzzle().getHeight(); i++) {
-      if (getActivePuzzle().getCellType(i, c) != CellType.WALL && lamps[i][c]) {
+    for (int i = r;
+        i < getActivePuzzle().getHeight() && getActivePuzzle().getCellType(i, c) != CellType.WALL;
+        i++) {
+      if (lamps[i][c]) {
         return true;
       }
     }
-    for (int j = c; j >= 0; j--) {
-      if (getActivePuzzle().getCellType(r, j) != CellType.WALL && lamps[r][j]) {
+    for (int j = c; j >= 0 && getActivePuzzle().getCellType(r, j) != CellType.WALL; j--) {
+      if (lamps[r][j]) {
         return true;
       }
     }
-    for (int j = c; j < getActivePuzzle().getWidth(); j++) {
-      if (getActivePuzzle().getCellType(r, j) != CellType.WALL && lamps[r][j]) {
+    for (int j = c;
+        j < getActivePuzzle().getWidth() && getActivePuzzle().getCellType(r, j) != CellType.WALL;
+        j++) {
+      if (lamps[r][j]) {
         return true;
       }
     }
@@ -108,95 +112,6 @@ public class ModelImpl implements Model {
     return lamps[r][c];
   }
 
-  /*
-  @Override
-  public boolean isLampIllegal(int r, int c) {
-    validatePosition(r, c);
-    if (!lamps[r][c]) {
-      return false;
-    }
-
-    return checkDirectionLamp(r, c, -1, 0)
-        || checkDirectionLamp(r, c, 1, 0)
-        || checkDirectionLamp(r, c, 0, -1)
-        || checkDirectionLamp(r, c, 0, 1);
-  }
-  */
-
-  /*
-  @Override
-  public boolean isLampIllegal(int r, int c) {
-    validatePosition(r, c);
-    if (!lamps[r][c]) {
-      throw new IllegalArgumentException("no lamp at this position");
-    }
-
-    for (int i = 0; i < r; i++) {
-      if (getActivePuzzle().getCellType(i, c) == CellType.CORRIDOR && lamps[i][c]) {
-        return true;
-      }
-      if (getActivePuzzle().getCellType(i, c) == CellType.WALL
-          || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
-        break;
-      }
-    }
-    for (int i = r + 1; i < getActivePuzzle().getHeight(); i++) {
-      if (getActivePuzzle().getCellType(i, c) == CellType.CORRIDOR && lamps[i][c]) {
-        return true;
-      }
-      if (getActivePuzzle().getCellType(i, c) == CellType.WALL
-          || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
-        break;
-      }
-    }
-    for (int j = 0; j < c; j++) {
-      if (getActivePuzzle().getCellType(r, j) == CellType.CORRIDOR && lamps[r][j]) {
-        return true;
-      }
-      if (getActivePuzzle().getCellType(r, j) == CellType.WALL
-          || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
-        break;
-      }
-    }
-    for (int j = c + 1; j < getActivePuzzle().getWidth(); j++) {
-      if (getActivePuzzle().getCellType(r, j) == CellType.CORRIDOR && lamps[r][j]) {
-        return true;
-      }
-      if (getActivePuzzle().getCellType(r, j) == CellType.WALL
-          || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
-        break;
-      }
-    }
-
-    return false;
-  }
-  */
-
-  /*
-  @Override
-  public boolean isLampIllegal(int r, int c) {
-    validatePosition(r, c);
-    if (!lamps[r][c]) {
-      throw new IllegalArgumentException("no lamp at this position");
-    }
-
-    if (r > 0 && getActivePuzzle().getCellType(r - 1, c) == CellType.CORRIDOR && lamps[r - 1][c]) {
-      return true;
-    }
-    if (r < getActivePuzzle().getHeight() - 1 && getActivePuzzle().getCellType(r + 1, c) == CellType.CORRIDOR && lamps[r + 1][c]) {
-      return true;
-    }
-    if (c > 0 && getActivePuzzle().getCellType(r, c - 1) == CellType.CORRIDOR && lamps[r][c - 1]) {
-      return true;
-    }
-    if (c < getActivePuzzle().getWidth() - 1 && getActivePuzzle().getCellType(r, c + 1) == CellType.CORRIDOR && lamps[r][c + 1]) {
-      return true;
-    }
-
-    return false;
-  }
-  */
-
   @Override
   public boolean isLampIllegal(int r, int c) {
     if (!isLamp(r, c)) {
@@ -204,7 +119,8 @@ public class ModelImpl implements Model {
     }
 
     for (int i = r - 1; i >= 0; i--) {
-      if (getActivePuzzle().getCellType(i, c) == CellType.WALL || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
+      if (getActivePuzzle().getCellType(i, c) == CellType.WALL
+          || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
         break;
       }
       if (lamps[i][c]) {
@@ -212,7 +128,8 @@ public class ModelImpl implements Model {
       }
     }
     for (int i = r + 1; i < getActivePuzzle().getHeight(); i++) {
-      if (getActivePuzzle().getCellType(i, c) == CellType.WALL || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
+      if (getActivePuzzle().getCellType(i, c) == CellType.WALL
+          || getActivePuzzle().getCellType(i, c) == CellType.CLUE) {
         break;
       }
       if (lamps[i][c]) {
@@ -220,7 +137,8 @@ public class ModelImpl implements Model {
       }
     }
     for (int j = c - 1; j >= 0; j--) {
-      if (getActivePuzzle().getCellType(r, j) == CellType.WALL || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
+      if (getActivePuzzle().getCellType(r, j) == CellType.WALL
+          || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
         break;
       }
       if (lamps[r][j]) {
@@ -228,7 +146,8 @@ public class ModelImpl implements Model {
       }
     }
     for (int j = c + 1; j < getActivePuzzle().getWidth(); j++) {
-      if (getActivePuzzle().getCellType(r, j) == CellType.WALL || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
+      if (getActivePuzzle().getCellType(r, j) == CellType.WALL
+          || getActivePuzzle().getCellType(r, j) == CellType.CLUE) {
         break;
       }
       if (lamps[r][j]) {
